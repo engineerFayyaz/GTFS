4285f65e2200cae60ad427f8543d139d8ca955e7
codesinc-repo <codes.inc.com@gmail.com>
2024-05-21 15:10:46 +0500

    save the route |css|otherthings

--- src/FirebaseConfig.jsx
+++ src/FirebaseConfig.jsx
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBvqUHysbjYNn3nAg8pNhD8eA0LmbSOlX0",
   authDomain: "bus-driver-navigation.firebaseapp.com",
@@ -20,6 +18,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-
-export { app, analytics };
+export { app, analytics, db };
